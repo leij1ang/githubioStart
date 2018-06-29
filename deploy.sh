ecea01b5b24914efed3cee83c9974aada7e1c767
lei.jiang <jianglei19940909@126.com>
2018-06-29 19:10:00 +0800

    1:add variable "repo" 2:fix bugs

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,26 @@
 sourceRepo={{}}
 userName={{}}
 thisPwd=`pwd`
-cd ./repo/${sourceRepo}
+repo="${thisPwd}/repo"
+cd ${repo}/${sourceRepo}
 git fetch --all
 git reset --hard origin/master
 #根据项目需求修改以下
 npm i
 npm run build:prod
-rm -rf ${thisPwd}/repo/${userName}.github.io/${sourceRepo}/*
+rm -rf ${repo}/${userName}.github.io/${sourceRepo}/*
 shopt -s expand_aliases
-if [[ `alias|grep` != '' ]]; then
+if [[ `alias|grep cp` != '' ]]; then
     unalias cp
-    cp -rf dist/* ${thisPwd}/repo/${userName}.github.io/${sourceRepo}
+    cp -rf dist/* ${repo}/${userName}.github.io/${sourceRepo}
     alias cp='cp -i'
 else
-    cp -rf dist/* ${thisPwd}/repo/${userName}.github.io/${sourceRepo}
+    cp -rf dist/* ${repo}/${userName}.github.io/${sourceRepo}
 fi
 
 #根据项目需求修改以上
 gitlog=`git log --oneline -1`
-cd ${thisPwd}/repo/${userName}.github.io/
+cd ${repo}/${userName}.github.io/
 git add .
 git commit -am "${gitlog}"
 git push --force
--- init.sh
+++ init.sh
@@ -3,18 +3,19 @@ read -p "please input the source repo name : " sourceRepo
 read -p "please input your userName : " userName
 read -p "please input the hook port : " hookPort
 thisPwd=`pwd`
+repo="${thisPwd}/repo"
 sed -i "s/^sourceRepo=.*$/sourceRepo=${sourceRepo}/" ./deploy.sh
 sed -i "s/^userName=.*$/userName=${userName}/" ./deploy.sh
 sed -i "s/^const hookPort=.*$/hookPort=${hookPort}/" ./deploy.js
 npm i exec &
-if [[ -d ${thisPwd}/repo ]]; then
-    rm -rf ${thisPwd}/repo
+if [[ -d ${repo} ]]; then
+    rm -rf ${repo}
 fi
-mkdir -p ${thisPwd}/repo/${sourceRepo} ${userName}.github.io/${sourceRepo}
-cd ${thisPwd}/repo/${sourceRepo}
+mkdir -p ${repo}/${sourceRepo} ${userName}.github.io/${sourceRepo}
+cd ${repo}/${sourceRepo}
 git init
 git remote add origin git@github.com:${userName}/${sourceRepo}.git
-cd ${thisPwd}/repo/${userName}.github.io
+cd ${repo}/${userName}.github.io
 git init
 git remote add origin git@github.com:${userName}/${userName}.git
 cd ${thisPwd}
